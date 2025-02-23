5b54c8e126bf985261e8e64f0c15e098275a3371
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-02-23 10:17:43 +0000

    get commentary pechas mapping

--- src/alignment_ann_transfer/commentary.py
+++ src/alignment_ann_transfer/commentary.py
@@ -8,6 +8,26 @@ from alignment_ann_transfer.utils import parse_root_mapping
 
 
 class CommentaryAlignmentTransfer(AlignmentTransfer):
+    def get_commentary_pechas_mapping(
+        self, commentary_pecha: Pecha, commentary_display_pecha: Pecha
+    ) -> Dict[int, List]:
+        """
+        Get Segmentation mapping from commentary display pecha -> commentary pecha(root idx mapping)
+        """
+        display_layer_path = self.get_first_layer_path(commentary_pecha)
+        new_tgt_layer_path = self.base_update(
+            commentary_display_pecha, commentary_pecha
+        )
+
+        display_layer = AnnotationStore(file=str(display_layer_path))
+        transfer_layer = AnnotationStore(file=str(new_tgt_layer_path))
+
+        map = self.map_commentary_layer_to_layer(transfer_layer, display_layer)
+
+        # Clean up the layer
+        new_tgt_layer_path.unlink()
+        return map
+
     def get_serialized_commentary(
         self, root_pecha: Pecha, root_display_pecha: Pecha, commentary_pecha: Pecha
     ):
@@ -30,6 +50,36 @@ class CommentaryAlignmentTransfer(AlignmentTransfer):
             segments.append(f"<1><{display_idx}>{commentary_text}")
         return segments
 
+    # def get_serialized_commentary_display(
+    #     self,
+    #     root_pecha: Pecha,
+    #     root_display_pecha: Pecha,
+    #     commentary_pecha: Pecha,
+    #     commentary_display_pecha: Pecha,
+    # ):
+    #     """
+    #     Input: map from transfer_layer -> display_layer (One to Many)
+    #     Structure in a way such as : <chapter number><display idx>translation text
+    #     Note: From many relation in display layer, take first idx (Sefaria map limitation)
+    #     """
+    #     root_map = self.get_root_pechas_mapping(root_pecha, root_display_pecha)
+    #     translation_map = self.get_commentary_pechas_mapping(
+    #         self.commentary_pecha, self.commentary_display_pecha
+    #     )
+
+    #     layer_path = next(commentary_display_pecha.layer_path.rglob("*.json"))
+
+    #     anns = self.extract_anns(AnnotationStore(file=str(layer_path)))
+
+    #     segments = []
+    #     for src_idx, tgt_map in translation_map.items():
+    #         translation_text = anns[src_idx]["text"]
+    #         tgt_idx = tgt_map[0][0]
+
+    #         root_idx = root_map[tgt_idx][0][0]
+    #         segments.append(f"<1><{root_idx}>{translation_text}")
+    #     return segments
+
     def extract_commentary_anns(self, layer: AnnotationStore) -> List[Dict]:
         """
         Extract annotation from layer(STAM)
@@ -48,3 +98,45 @@ class CommentaryAlignmentTransfer(AlignmentTransfer):
                 }
             )
         return anns
+
+    def map_commentary_layer_to_layer(
+        self, src_layer: AnnotationStore, tgt_layer: AnnotationStore
+    ):
+        """
+        1. Extract annotations from source and target layers
+        2. Map the annotations from source to target layer
+        src_layer -> tgt_layer (One to Many)
+        """
+        mapping: Dict = {}
+
+        src_anns = self.extract_commentary_anns(src_layer)
+        tgt_anns = self.extract_commentary_anns(tgt_layer)
+
+        for src_ann in src_anns:
+            src_start, src_end = src_ann["Span"]["start"], src_ann["Span"]["end"]
+            src_idx = int(src_ann["root_idx_mapping"])
+            mapping[src_idx] = []
+
+            for tgt_ann in tgt_anns:
+                tgt_start, tgt_end = tgt_ann["Span"]["start"], tgt_ann["Span"]["end"]
+                tgt_indicies = parse_root_mapping(tgt_ann["root_idx_mapping"])
+                tgt_idx = tgt_indicies[0]  # Take the first index
+
+                # Check for mapping conditions
+                if (
+                    src_start
+                    <= tgt_start
+                    < src_end  # Target annotation starts within source
+                    or src_start
+                    < tgt_end
+                    <= src_end  # Target annotation ends within source
+                    or (
+                        tgt_start < src_start and tgt_end > src_end
+                    )  # Target fully contains source
+                ) and not (
+                    tgt_start == src_end or tgt_end == src_start
+                ):  # No exact edge overlap
+                    mapping[src_idx].append([tgt_idx, [tgt_start, tgt_end]])
+
+        # Sort the mapping by source indices
+        return dict(sorted(mapping.items()))
--- tests/commentary/test_commentary_ann_transfer.py
+++ tests/commentary/test_commentary_ann_transfer.py
@@ -14,6 +14,7 @@ class TestCommentaryAlignmentTransfer(TestCase):
         self.root_pecha = Pecha.from_path(DATA_DIR / "P2/IC7760088")
         self.root_display_pecha = Pecha.from_path(DATA_DIR / "P1/IA6E66F92")
         self.commentary_pecha = Pecha.from_path(DATA_DIR / "P3/I77BD6EA9")
+        self.commentary_display_pecha = Pecha.from_path(DATA_DIR / "P4/IE292A440")
 
     def test_get_root_pechas_mapping(self):
         commentary_transfer = CommentaryAlignmentTransfer()
@@ -23,6 +24,14 @@ class TestCommentaryAlignmentTransfer(TestCase):
         expected_mapping = read_json(DATA_DIR / "root_pechas_mapping.json")
         assert {str(k): v for k, v in mapping.items()} == expected_mapping
 
+    def test_get_commentary_pechas_mapping(self):
+        commentary_transfer = CommentaryAlignmentTransfer()
+        mapping = commentary_transfer.get_commentary_pechas_mapping(
+            self.commentary_pecha, self.commentary_display_pecha
+        )
+        expected_mapping = read_json(DATA_DIR / "commentary_pechas_mapping.json")
+        assert {str(k): v for k, v in mapping.items()} == expected_mapping
+
     def test_get_serialized_commentary(self):
         commentary_transfer = CommentaryAlignmentTransfer()
         serialized_json = commentary_transfer.get_serialized_commentary(
@@ -30,3 +39,8 @@ class TestCommentaryAlignmentTransfer(TestCase):
         )
         expected_serialized_json = read_json(DATA_DIR / "serialized_commentary.json")
         assert serialized_json == expected_serialized_json
+
+
+work = TestCommentaryAlignmentTransfer()
+work.setUp()
+work.test_get_commentary_pechas_mapping()
